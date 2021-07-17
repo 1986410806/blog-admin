9851b5db47ed7b8541b8a9b1d4c27e38d8e04909
zhaohuinan <1986410806@qq.com>
2021-07-18 03:27:15 +0800

    debug

---
+++ src/pages/dashboard/workplace/service.js
@@ -0,0 +1,13 @@
+import { request } from 'umi';
+export async function queryProjectNotice() {
+  return request('/api/project/notice');
+}
+export async function queryActivities() {
+  return request('/api/activities');
+}
+export async function fakeChartData() {
+  return request('/api/fake_workplace_chart_data');
+}
+export async function queryCurrent() {
+  return request('/api/currentUser');
+}
